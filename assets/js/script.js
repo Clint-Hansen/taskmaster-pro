63c9dc4ca9161d1981c233f2138d3b1b5c85819c
Clint Hansen <clint.hansen2009@gmail.com>
2021-02-18 14:35:24 -0700

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -167,12 +167,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
+    $(event.target).addClass("dropover-active")
   },
   out: function(event) {
+    $(event.target).removeClass("dropover-active")
   },
   update: function(event) {
     // array to store the task data in
@@ -211,9 +217,13 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active")
   },
   out: function(event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active")
+  },
+  over: function(event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
   }
 });
 
@@ -237,7 +247,7 @@ $("#modalDueDate").datepicker({
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
